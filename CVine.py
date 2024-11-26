44e83b0aba5ce5ef6204d544a8a66e0e69641265
tyyyyyy <tyyzltrt@126.com>
2024-11-25 23:49:04 -0500

    added DataFetcher, Gaussian and reverse function

--- CVine.py
+++ CVine.py
@@ -1,69 +1,77 @@
 import numpy as np
 from scipy.optimize import minimize
-from copula import Clayton
+from copula import Clayton, Gaussian
+from distribution import Multivariate
+from data_fetcher import DataFetcher
+
 
 class CVine(object):
-    layer = {"root": [], # list of root nodes. ex. in F(u1, u2|v), v is the root node 
-             "parentnode": {}, # index of nodes in last level. ex. {1: (1,2)} means the node 1 in this tree level got from the node pair (1,2) in last level
-             "node": [], # index of the nodes. from 0 to l. this is not the actual number index in the original data.
-             "pair": [],# list of node pairs in the tree, ex. in F(u1, u2|v), (u1, u2) is a node pair. here the node pair is the index of nodes in the root, which is different from the "node". 
-             "level": 0, # level of the tree (k). 0-root, 1-1st level, 2-2nd level, ...
-             "nodenum": 0, # number of the nodes in this tree (l). equal to n - k 
-             "edgenum": 0, # number of the edges in this tree. equal to l as our node number is the actual number minus1.
+    layer = {"root": [],  # list of root nodes. ex. in F(u1, u2|v), v is the root node
+             "parentnode": {},
+             # index of nodes in last level. ex. {1: (1,2)} means the node 1 in this tree level got from the node pair (1,2) in last level
+             "node": [],  # index of the nodes. from 0 to l. this is not the actual number index in the original data.
+             "pair": [],
+             # list of node pairs in the tree, ex. in F(u1, u2|v), (u1, u2) is a node pair. here the node pair is the index of nodes in the root, which is different from the "node".
+             "level": 0,  # level of the tree (k). 0-root, 1-1st level, 2-2nd level, ...
+             "nodenum": 0,  # number of the nodes in this tree (l). equal to n - k
+             "edgenum": 0,
+             # number of the edges in this tree. equal to l as our node number is the actual number minus1.
              "V": None,  # h functions in this level. V[:, j] is the h function of node j.
-             "dependence": [] # match the pair of nodes
-             }    
+             "dependence": []  # match the pair of nodes
+             }
 
-    tree = {"thetaMatrix": None, # copula parameter matrix in this level. it is a upper matrix. thetaMatrix[i, j] is the copula parameter in level j between node 1 and node i+1. 
-            "structure": {}, # the tree structure in this level. the key is the node index, the value is layer.
-            "depth": 0, # the depth of the tree, 0 means only has root. 
+    tree = {"thetaMatrix": None,
+            # copula parameter matrix in this level. it is a upper matrix. thetaMatrix[i, j] is the copula parameter in level j between node 1 and node i+1.
+            "structure": {},  # the tree structure in this level. the key is the node index, the value is layer.
+            "depth": 0,  # the depth of the tree, 0 means only has root.
             }
-    max_depth = None # if the max_depth is given, the only use the first max_depth layers to fit the model. 
+    max_depth = None  # if the max_depth is given, the only use the first max_depth layers to fit the model.
 
-    def __init__(self, U, copulaType="Clayton",dependence=None, max_depth=None):
-        
+    def __init__(self, U, copulaType="Clayton", dependence=None, max_depth=None):
+
         """
         U: np.array, data matrix. follows uniform distribution
-        dependence: np.array, dependence matrix constructed by 0 and 1. dependence[i, j] = 1 means i and j are dependent; 0 means independent. 
+        dependence: np.array, dependence matrix constructed by 0 and 1. dependence[i, j] = 1 means i and j are dependent; 0 means independent.
 
         """
         self.U = U
         self.T = U.shape[0]
-        self.variable_num = U.shape[1] - 1  # to make the structure more clear, all the variables are indexed from 0. Therefore, when the variable_num is n, we actually have n+1 variables x0, x1, ..., xn.
-        self.dependence = dependence # if dependence is None, we assume all the variables are dependen . if not, the copula function between independent variables will be 1.
+        self.variable_num = U.shape[
+                                1] - 1  # to make the structure more clear, all the variables are indexed from 0. Therefore, when the variable_num is n, we actually have n+1 variables x0, x1, ..., xn.
+        self.dependence = dependence  # if dependence is None, we assume all the variables are dependen . if not, the copula function between independent variables will be 1.
         if copulaType == "Clayton":
             self.copula = Clayton()
+        elif copulaType == "Gaussian":
+            self.copula = Gaussian()
         else:
             raise ValueError("The copula type is not supported.")
 
-        self.max_depth = max_depth if max_depth is not None else self.variable_num
+        self.max_depth = max_depth
 
-        
     def build_tree(self):
         """
-        build the tree structure. 
+        build the tree structure.
         """
         self.build_root()
-        while self.tree["depth"] < self.max_depth:
+        while self.tree["depth"] < self.variable_num:
             self.build_kth_tree()
 
     def build_root(self):
         """
-        build the root of the tree. the root is basically 
+        build the root of the tree. the root is basically
         """
-        
+
         layer = self.layer.copy()
         layer["level"] = 0
-        layer["V"] = self.U.copy() # the F(x|v) in the first layer is the empirical cdf of x. 
+        layer["V"] = self.U.copy()  # the F(x|v) in the first layer is the empirical cdf of x.
         layer["nodenum"] = self.variable_num
-        layer["edgenum"] = self.variable_num 
+        layer["edgenum"] = self.variable_num
         layer["node"] = list(range(0, layer["nodenum"] + 1))
         self.tree["structure"][0] = layer
 
-        
     def build_kth_tree(self):
         """
-        build the kth tree. 
+        build the kth tree.
         """
 
         if self.tree["depth"] >= self.variable_num:
@@ -75,51 +83,52 @@ class CVine(object):
         layer["level"] = last_layer["level"] + 1
         layer["nodenum"] = last_layer["nodenum"] - 1
         layer["edgenum"] = layer["nodenum"]
-        layer["node"] = list(range(0, layer["nodenum"]+ 1))
+        layer["node"] = list(range(0, layer["nodenum"] + 1))
         (layer["pair"], layer["node"], layer["parentnode"], layer["root"]) = self.pair_nodes(last_layer)
 
         # todo dependence
 
-
         self.tree["structure"][layer["level"]] = layer
         self.tree["depth"] = self.tree["depth"] + 1
-    
+
     def pair_nodes(self, last_layer):
         """
-        pair the nodes in this layer. 
+        pair the nodes in this layer.
         here we use the first node in each level as the new central node and combine it with the root in last level to get the new root. This process is same as the process we show in the report.
         """
 
-
         nodes = range(0, last_layer["nodenum"] + 1)
 
-        if last_layer["level"] == 0: # the second layer is not conditional copula, so we just combine the center node with neighor nodes without any condition.
+        if last_layer[
+            "level"] == 0:  # the second layer is not conditional copula, so we just combine the center node with neighor nodes without any condition.
             pair_left = last_layer["node"][0]
             pairs = tuple(zip(last_layer["edgenum"] * [pair_left], last_layer["node"][1:]))
             parentnodes = dict(zip(nodes, pairs))
-            return (pairs, 
-                    nodes, 
-                    parentnodes, 
+            return (pairs,
+                    nodes,
+                    parentnodes,
                     [])
         else:
             pairs = []
             parentnodes = {}
             last_pairs = last_layer["pair"]
-            
-            common_node = last_pairs[0][0] # always set the first node as the center node in each layer. and the common element between pairs are the left element in the center pair. the common element between the center node and neighbor nodes will be set into the new root.
+
+            common_node = last_pairs[0][
+                0]  # always set the first node as the center node in each layer. and the common element between pairs are the left element in the center pair. the common element between the center node and neighbor nodes will be set into the new root.
 
             new_root = last_layer["root"] + [common_node]
-            pair_left = last_pairs[0][1] # the right element in the center pair will be the left element in pairs in this layer. 
+            pair_left = last_pairs[0][
+                1]  # the right element in the center pair will be the left element in pairs in this layer.
 
             for i in range(1, last_layer["nodenum"] + 1):
                 pairs.append(tuple((pair_left, last_pairs[i][1])))
-                parentnodes[i-1] = (0, i)  # the i-1th node in this layer is from the pair (0, i) in last layer. ex. the first layer in the second layer is from the node pair (0, 1) in the first layer.
-                
-        return (pairs, 
-                nodes,
-                parentnodes, 
-                new_root)
+                parentnodes[i - 1] = (0,
+                                      i)  # the i-1th node in this layer is from the pair (0, i) in last layer. ex. the first layer in the second layer is from the node pair (0, 1) in the first layer.
 
+        return (pairs,
+                nodes,
+                parentnodes,
+                new_root)
 
     def fit(self):
         """
@@ -141,26 +150,22 @@ class CVine(object):
 
         self.tree["thetaMatrix"] = thetaMatrix
 
-
     def fit2(self):
-        
+
         """
         fit the parameters through maximizing the likelihood in each layer.
         """
         self.tree["thetaMatrix"] = np.zeros((self.tree["depth"], self.tree["structure"][0]["edgenum"]))
 
-        for i in range(1, self.tree["depth"]+1):
-            last_layer = self.tree["structure"][i-1]
+        for i in range(1, self.tree["depth"] + 1):
+            last_layer = self.tree["structure"][i - 1]
             layertheta = np.ones(last_layer["edgenum"])
             bounds = [self.copula.bound] * last_layer["edgenum"]
-            result = minimize(self.get_layer_likelihood, layertheta, args=(last_layer, ), bounds=bounds)
+            result = minimize(self.get_layer_likelihood, layertheta, args=(last_layer,), bounds=bounds)
 
-            self.tree["thetaMatrix"][i-1, :last_layer["edgenum"]] = result.x
+            self.tree["thetaMatrix"][i - 1, :last_layer["edgenum"]] = result.x
 
             self.tree["structure"][i]["V"] = self.get_layer_h(result.x, last_layer)
-
-
-        print("thetaMatrix", self.tree["thetaMatrix"])
 
     def simulate(self, n):
         """
@@ -171,46 +176,42 @@ class CVine(object):
         if self.tree["thetaMatrix"] is None:
             print("Please fit the model first.")
             return None
-        
-        else:
 
+        else:
             W = np.random.uniform(0, 1, n * (self.variable_num + 1))
-            V = np.empty((n, self.variable_num+1, self.variable_num+1))
+            V = np.empty((n, self.variable_num + 1, self.tree["depth"] + 1))
             W = W.reshape((n, self.variable_num + 1))
             U = np.empty((n, self.variable_num + 1))
             U[:, 0] = W[:, 0]
-            V[:, 0, 0] = W[:, 0] 
-            c = min(self.tree["depth"], self.variable_num)
+            V[:, 0, 0] = W[:, 0]
             for i in range(1, self.variable_num + 1):
                 V[:, 0, i] = W[:, i]
-                for k in range(0, min(i, c)):
-                    self.copula.theta = self.tree["thetaMatrix"][k, i-k-1]
+                for k in range(0, i):
+                    self.copula.theta = self.tree["thetaMatrix"][k, i - k - 1]
                     V[:, 0, i] = self.copula.inverse_h(V[:, 0, i], V[:, k, k])
-                
+
                 U[:, i] = V[:, 0, i]
 
-                for j in range(0, min(i,c)): 
-                    self.copula.theta = self.tree["thetaMatrix"][j, i-j-1]
+                for j in range(0, i):
+                    self.copula.theta = self.tree["thetaMatrix"][j, i - j - 1]
                     V[:, j + 1, i] = self.copula.h(V[:, j, i], V[:, j, j])
-            
             return U
-        
 
     def get_likelihood(self, thetaParams):
         """get the likelihood of the vine tree model"""
-        
+
         total_likelihood = 0
-        left = 0 
+        left = 0
         right = 0
-        for k in range(1, self.tree["depth"] + 1): # ignore the root layer
+        for k in range(1, self.tree["depth"] + 1):  # ignore the root layer
             # each layer' c function is determined by the last layer's and this layer's theta. number of theta in each layer is equal to the number of nodes in this layer.
-            last_layer = self.tree["structure"][k-1]
+            last_layer = self.tree["structure"][k - 1]
 
             left = right
-            right = right + last_layer["edgenum"] 
+            right = right + last_layer["edgenum"]
             layertheta = thetaParams[left:right]
             total_likelihood += self.get_layer_likelihood(layertheta, last_layer)
-            
+
             self.tree["structure"][k]["V"] = self.get_layer_h(layertheta, last_layer)
 
         return total_likelihood
@@ -218,23 +219,45 @@ class CVine(object):
     def get_layer_likelihood(self, thetaParams, last_layer):
         """get the likelihood of the layer"""
         likelihood = 0
-        
-        for i in range(1, last_layer["nodenum"]+1): # totally l copula functions
 
-            self.copula.theta = thetaParams[i-1]
+        for i in range(1, last_layer["nodenum"] + 1):  # totally l copula functions
+
+            self.copula.theta = thetaParams[i - 1]
             likelihood += np.nansum(np.log(self.copula.c(last_layer["V"][:, 0], last_layer["V"][:, i])))
-        
+
         return -likelihood
 
     def get_layer_h(self, thetaParams, last_layer):
         """get the h function of the layer"""
-        V = np.empty((self.T, last_layer["nodenum"])) # the total nodes of this layer is the number of nodes in last layer minus 1, which is equal to the edges in last layer.
-        
-        for i in range(1, last_layer["nodenum"]+1):
-            self.copula.theta = thetaParams[i-1]
-            V[:, i-1] = self.copula.h(last_layer["V"][:, 0], last_layer["V"][:, i])
-        
+        V = np.empty((self.T, last_layer[
+            "nodenum"]))  # the total nodes of this layer is the number of nodes in last layer minus 1, which is equal to the edges in last layer.
+
+        for i in range(1, last_layer["nodenum"] + 1):
+            self.copula.theta = thetaParams[i - 1]
+            V[:, i - 1] = self.copula.h(last_layer["V"][:, 0], last_layer["V"][:, i])
+
         return V
 
-    
+
+if __name__ == "__main__":
+    tickers = ['^GSPC', '^DJI', '^TNX', '^IXIC', '^RUT']
+    start_date = '2023-01-01'
+    end_date = '2024-11-01'
+
+    dft = DataFetcher(tickers=tickers, start_date=start_date, end_date=end_date)
+    dft.fetch_and_save_data()
+    dft.plot_distribuion()
+    y = dft.data
+    dataproc = Multivariate(y)
+    u = dataproc.empircal_cdf()
+    u = u.values
+    cv = CVine(u, copulaType="Gaussian")
+    cv.build_tree()
+    cv.fit()
+    simulated_data = cv.simulate(1000)
+    # todo: check the overflow
+    simulated_data = simulated_data.clip(0, 1)
+    simulated_data[np.isnan(simulated_data)] = 0
+    simulated_data_reversed = dataproc.empircal_ppf(simulated_data)
+    print(cv.tree["thetaMatrix"])
 
--- data_fetcher.py
+++ data_fetcher.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import os
+from matplotlib import pyplot as plt
+import math
 
 
 class DataFetcher:
@@ -10,7 +12,7 @@ class DataFetcher:
     Fetch historical data from yfinance and save it locally.
     """
 
-    def __init__(self, tickers: list, start_date: datetime, end_date: datetime, interval: str = "1d"):
+    def __init__(self, tickers: list, start_date: datetime, end_date: datetime):
         """
         Initialize DataFetcher.
 
@@ -18,12 +20,10 @@ class DataFetcher:
         - tickers (list): List of stock/index tickers.
         - start_date (datetime): Start date.
         - end_date (datetime): End date.
-        - interval (str): Data interval (default "1d").
         """
         self.tickers = tickers
         self.start_date = start_date
         self.end_date = end_date
-        self.interval = interval
         self.base_path = os.getcwd()
         self.data_dir = os.path.join(self.base_path, "Data")
         os.makedirs(self.data_dir, exist_ok=True)
@@ -32,49 +32,61 @@ class DataFetcher:
         """
         Fetch historical data for each ticker and save as CSV.
         """
-        print(f"Data for tickers {', '.join(self.tickers)} saved to {self.data_dir}.")
-        print()
+        file_path = os.path.join(self.data_dir, "Index_data.csv")
+        if os.path.exists(file_path):
+            close = pd.read_csv(file_path)
 
-        
+        else:
+            data = yf.download(self.tickers, start=self.start_date, end=self.end_date)['Adj Close']
+            data = data.loc[:, self.tickers]
+            data.ffill(inplace=True)
+            data.to_csv(file_path)
+
+        if os.path.exists(file_path):
+             close = pd.read_csv(file_path, index_col=0)
+
+        self.data = close.pct_change().dropna(how='any')
+
+    def plot_distribuion(self):
         for ticker in self.tickers:
-            print(f"Fetching data for {ticker}...")
-            stock = yf.Ticker(ticker)
-            df = stock.history(start=self.start_date, end=self.end_date, interval=self.interval)
+            return_data = np.array(self.data[ticker])
+            plt.figure(figsize=(8, 6), dpi=100)
+            plt.hist(return_data, bins=50, density=True, color='skyblue', edgecolor='black')
+            plt.title(ticker)
+            plt.plot()
 
-            if df.empty:
-                print(f"No data found for {ticker}. Skipping.")
-                continue
 
-            df = df.drop(columns=['Dividends', 'Stock Splits'], errors='ignore')
-            safe_ticker = ticker.replace('^', '')
-            file_path = os.path.join(self.data_dir, f"{safe_ticker}_data.csv")
-            df.to_csv(file_path)
-            
-        print()
 
-            
 
-    def load_and_calculate_returns(self) -> pd.DataFrame:
-        """
-        Load saved CSVs and calculate returns.
+# def generate_normal_bm(miu, sigma, n):
+#     # generate D ~ Exp(1 / 2)
+#     d = - 2 * np.log(np.random.uniform(0, 1, int(n / 2)))
+#
+#     # generate Θ ~ Unif(0, 2Π)
+#     theta = 2 * math.pi * np.random.uniform(0, 1, int(n / 2))
+#
+#     # generate X, Y ~ Normal(miu, sigma)
+#     X = np.sqrt(d) * np.cos(theta) * sigma + miu
+#     Y = np.sqrt(d) * np.sin(theta) * sigma + miu
+#     normal_random_variables = np.hstack((X, Y))
+#     return normal_random_variables
 
-        Returns:
-        - pd.DataFrame: Combined returns data.
-        """
-        files = [os.path.join(self.data_dir, f"{ticker.replace('^', '')}_data.csv") for ticker in self.tickers]
-        dataframes = {}
 
-        for file in files:
-            asset_name = os.path.basename(file).replace('_data.csv', '')
-            df = pd.read_csv(file, parse_dates=['Date'], index_col='Date')
-            df['Returns'] = 100 * df['Close'].pct_change()
-            df['log_Returns'] = np.log(df['Close'] / df['Close'].shift(1))
-            df.dropna(inplace=True)
-            dataframes[asset_name] = df
-            print(f"Processed data for {asset_name}, shape: {df.shape}.")
+# parameter_dict = {}
+# distribution_dict = {}
+# n = 100
 
-        returns_data = {name: df['Returns'] for name, df in dataframes.items()}
-        combined_data = pd.concat(returns_data, axis=1).dropna()
-        combined_data.columns = dataframes.keys()
 
-        return combined_data
+
+    # assume all the returns satisfies gaussian distribution, estimate the parameters
+    # miu = np.mean(return_data)
+    # sigma = np.std(return_data, ddof=0)
+    # parameter_dict[ticker] = [miu, sigma]
+    # # generate random variables from the estimated distribution
+    # random_number = generate_normal_bm(miu, sigma, n)
+    # distribution_dict[ticker] = random_number.copy()
+
+
+
+
+
